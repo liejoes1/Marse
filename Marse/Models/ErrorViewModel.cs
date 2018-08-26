e2df9978b6f29c29bd628d3493b4894a49fa0ef0
Joes lie <liejoes1@gmail.com>
2018-08-26 12:49:40 +0800

    Add project files.

---
+++ Marse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Marse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
